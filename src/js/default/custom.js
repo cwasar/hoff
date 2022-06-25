37e5ba9f05d3266bc6df466b3fd233fff3f63efa
cwasar <jeff73@mail.ru>
2022-06-25 19:47:59 +0300

    Добавил блок идеи для вдохновения

--- src/js/default/custom.js
+++ src/js/default/custom.js
@@ -44,3 +44,52 @@ $(".carousel").slick({
     dots: true
 })
 
+
+//блок Идеи для вдохновения
+const blocks = document.querySelector('.main-ideas-content-blocks')
+const block = document.querySelector('.main-ideas-content-block')
+blocks.addEventListener('mouseover', showPin)
+
+function showPin(e) {
+    let w = block.offsetWidth
+    let h = block.offsetHeight
+    if(e.target.classList.contains('main-ideas-content-block-pin')){
+        e.target.classList.add('active')
+        e.target.nextElementSibling.classList.add('active')
+
+        let pinH = e.target.style.top.substring(0, e.target.style.top.length - 1)
+        let pinW = e.target.style.left.substring(0, e.target.style.left.length - 1)
+
+        if(pinH < 50){
+            e.target.nextElementSibling.style.top = +pinH + 1 + '%'
+        } else {
+            e.target.nextElementSibling.style.top = +pinH + 1 - (e.target.nextElementSibling.offsetHeight * 100 / h) + '%'
+        }
+        if(pinW < 50){
+            e.target.nextElementSibling.style.left = +pinW + 1 + '%'
+        }else {
+            e.target.nextElementSibling.style.left = +pinW + 1 - (e.target.nextElementSibling.offsetWidth * 100 / w) + '%'
+        }
+
+    }
+    if(e.target.tagName === 'IMG'){
+        blocks.querySelectorAll('.active').forEach(item => {
+            item.classList.remove('active')
+        })
+
+    }
+
+}
+
+// blocks.addEventListener('mouseout', (e) => {
+//
+//     console.log(e.target)
+//         if(e.target.classList.contains('active')){
+//
+//         } else {
+//             e.target.classList.remove('active')
+//             e.target.nextElementSibling.classList.remove('active')
+//         }
+//
+//
+// })
--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,9 @@ module.exports = {
         }),
         new CopyWebpackPlugin({
             patterns: [
-                {from: path.resolve(__dirname, 'src/assets/img/main-slider/'), to: path.resolve(__dirname, 'dist')}
+                {from: path.resolve(__dirname, 'src/assets/img/main-slider/'), to: path.resolve(__dirname, 'dist')},
+                {from: path.resolve(__dirname, 'src/assets/img/ideas/'), to: path.resolve(__dirname, 'dist')},
+                {from: path.resolve(__dirname, 'src/assets/img/'), to: path.resolve(__dirname, 'dist')}
             ]
         })
 
